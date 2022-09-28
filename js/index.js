50dc2ada21444edf747247f219f62265942b373e
Victoria Chinellato <victoriach@live.com.ar>
2022-09-28 11:16:06 +0200

    half lab

--- js/index.js
+++ js/index.js
@@ -2,6 +2,12 @@
 
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
+  
+  let price = product.querySelector('.price span').innerHTML
+  let quantity = product.querySelector('.quantity input').value
+  let subtotal = quantity * price;
+
+  product.querySelector('.subtotal span').innerHTML = subtotal;
 
   //... your code goes here
 }
@@ -9,15 +15,36 @@ function updateSubtotal(product) {
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+
+
+  //const singleProduct = document.querySelector('.product');
+  //updateSubtotal(singleProduct);
+
+
   // end of test
 
   // ITERATION 2
-  //... your code goes here
+  let products = document.getElementsByClassName('product')
+  for ( i= 0; i < products.length; i++) {
+  let newPoroduct = products[i]
+  updateSubtotal(newPoroduct)
+  }
 
   // ITERATION 3
   //... your code goes here
+
+  let subtotales = document.querySelectorAll('.subtotal span').innerHTML;
+  let total = 0;
+  for (i = 0; i < subtotales.length; i++){
+  total += subtotales[i]
+  }
+  
+  document.querySelector('#total-value span').innerHTML =  total
+  
+
+  //let total = document.querySelector('#total-value span').innerHTML
+
+  
 }
 
 // ITERATION 4
