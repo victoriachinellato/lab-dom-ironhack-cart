49572274d2f5292718166a23c2b0bf3c9e96ebbe
Victoria Chinellato <victoriach@live.com.ar>
2022-10-07 22:03:28 +0200

    Solved lab

--- js/index.js
+++ js/index.js
@@ -1,13 +1,11 @@
 // ITERATION 1
 
 function updateSubtotal(product) {
-  console.log('Calculating subtotal, yey!');
-  
-  let price = product.querySelector('.price span').innerHTML
-  let quantity = product.querySelector('.quantity input').value
+  let price = product.querySelector('.price span').innerHTML;
+  let quantity = product.querySelector('.quantity input').value;
   let subtotal = quantity * price;
-
   product.querySelector('.subtotal span').innerHTML = subtotal;
+  return subtotal;
 
   //... your code goes here
 }
@@ -16,35 +14,24 @@ function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
 
-
   //const singleProduct = document.querySelector('.product');
   //updateSubtotal(singleProduct);
-
 
   // end of test
 
   // ITERATION 2
-  let products = document.getElementsByClassName('product')
-  for ( i= 0; i < products.length; i++) {
-  let newPoroduct = products[i]
-  updateSubtotal(newPoroduct)
+
+  let totalPrice = 0;
+  let products = document.getElementsByClassName('product');
+  for (i = 0; i < products.length; i++) {
+    let newPoroduct = products[i];
+    updateSubtotal(newPoroduct);
+    totalPrice += updateSubtotal(newPoroduct);
   }
 
   // ITERATION 3
-  //... your code goes here
-
-  let subtotales = document.querySelectorAll('.subtotal span').innerHTML;
-  let total = 0;
-  for (i = 0; i < subtotales.length; i++){
-  total += subtotales[i]
-  }
-  
-  document.querySelector('#total-value span').innerHTML =  total
-  
-
-  //let total = document.querySelector('#total-value span').innerHTML
-
-  
+  document.querySelector('#total-value span').innerHTML = totalPrice;
+  console.log(totalPrice);
 }
 
 // ITERATION 4
@@ -52,18 +39,66 @@ function calculateAll() {
 function removeProduct(event) {
   const target = event.currentTarget;
   console.log('The target in remove is:', target);
-  //... your code goes here
+  target.parentNode.parentNode.remove();
 }
 
 // ITERATION 5
 
 function createProduct() {
-  //... your code goes here
+  // 1.capturar los valores que luego formaran los nuevos productos:
+  const newProduct = document.querySelector('.create-product');
+  const newProductName = newProduct.querySelector('input[type="text"]').value;
+  console.log(newProductName);
+
+  //  para evitar que la funcion siga en cas de que el input este vacio:
+  if (newProductName.trim().length === 0) return;
+
+  const newProductPriceText = newProduct.querySelector('input[type="number"]').value;
+  const newProductPrice = parseFloat(newProductPriceText).toFixed(2);
+  console.log(newProductPrice);
+
+  // seleccionamos el elemento en el que agregaremos el producto nuevo
+  const body = document.querySelector('tbody');
+  body.innerHTML += `
+  <tr class="product">
+      <td class="name">
+        <span>${newProductName}</span>
+      </td>
+      <td class="price">$<span>${newProductPrice}</span></td>
+      <td class="quantity">
+        <input type="number" value="0" min="0" placeholder="Quantity" />
+      </td>
+      <td class="subtotal">$<span>0</span></td>
+      <td class="action">
+        <button class="btn btn-remove">Remove</button>
+      </td>
+</tr>`;
+
+// vacio el input una vez agregado el producto:
+newProduct.querySelector('input[type="text"]').value = "";
+newProduct.querySelector('input[type="number"]').value = 0;
+
+// volvemos a cargar los listerners que habiamos perdido
+removeListeners();
 }
+
+const removeListeners = () => {
+  // eliminar productos
+  const removeBtns = document.querySelectorAll('.btn.btn-remove');
+  for (let i = 0; i < removeBtns.length; i++) {
+    const element = removeBtns[i];
+    element.addEventListener('click', removeProduct);
+  }
+}
+
 
 window.addEventListener('load', () => {
   const calculatePricesBtn = document.getElementById('calculate');
   calculatePricesBtn.addEventListener('click', calculateAll);
 
-  //... your code goes here
+
+  // agregar productos:
+  // capturamos el evento de crear y le enviamos la funcion createProduct
+  removeListeners()
+  document.querySelector('#create').addEventListener('click', createProduct);
 });
